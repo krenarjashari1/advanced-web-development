1aeae1e12193795df49f83233336d7b59614a462
krenarjashari1 <72756184+krenarjashari1@users.noreply.github.com>
2021-04-07 15:13:00 +0200

    Update Gym Membership Controller

--- app/Http/Controllers/GymMembershipController.php
+++ app/Http/Controllers/GymMembershipController.php
@@ -12,17 +12,7 @@ class GymMembershipController extends Controller
 
 {
 
-
-
     public function createNewGymMember(Request $request){
-
-        $profilePicture = $request->file('profile_picture');
-        $path = null;
-
-        if($profilePicture != null){
-            $path = $profilePicture->store('public/images');
-            $path = str_replace("public/", 'storage/', $path);
-        }
 
         $newMember = new GymMembership();
         $newMember->first_name = $request->first_name;
@@ -30,40 +20,15 @@ class GymMembershipController extends Controller
         $newMember->birthdate = $request->birthdate;
         $newMember->expireDate = $request->expireDate;
 
-        $newMember->profile_picture = $path;
-        $newMember->is_active = true;
 
+        $newMember->profile_picture=$request->profile_picture;
         $newMember->save();
 
-        return redirect()->route('gym.membership');
-    }
-
-    public function addNewMember(){
-        return view('gymMembership');
+        return redirect()->route('viewGymMembers');
     }
 
 
 
 
-
-    public function addProfilePicture(Request $request, $first_name){
-        $newMember = GymMembership::find($first_name);
-
-        if(!$newMember){
-            return abort(404);
-        }
-
-        $profilePicture = $request->file('profile_picture');
-        $path = null;
-
-        if($profilePicture != null){
-            $path = $profilePicture->store('public/images');
-            $path = str_replace("public/", 'storage/', $path);
-        }
-
-        $newMember->profile_picture=$path;
-        $newMember->save();
-        return redirect(route('show.student', $newMember));
-    }
 
 }
